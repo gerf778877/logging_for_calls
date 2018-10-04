82c7b262b7eefc3af1f8abef2d57cf0812ad0707
vladimirsvsv77 <vladimirsvsv77@gmail.com>
2018-10-04 16:37:56 +0300

    changed logic, handle as json instead of parse args

--- api.py
+++ api.py
@@ -1,20 +1,19 @@
 #!flask/bin/python
+import json
 import uuid
 import urllib.request
 
 import werkzeug
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
 
-def write_log(request_id, file_name, log):
-    log_file = open('dialog_log.txt', 'a')
-    log_file.write('request_id: ' + request_id + ';\n')
-    log_file.write('file_name: ' + file_name + ';\n')
-    log_file.write('log: ' + log + ';\n\n')
+def write_log(json_data):
+    log_file = open('dialog_log.json', 'a')
+    json.dump(json_data + '\n', log_file)
     log_file.close()
 
 
@@ -31,22 +30,22 @@ class UploadAudio(Resource):
         audio_file.save('audio/' + request_id + '.mp3')
 
         dialog = args['dialog']
-        write_log(request_id, request_id + ".mp3", dialog)
+        write_log(dialog)
 
 
 class SaveVoxLog(Resource):
     def post(self):
         request_id = str(uuid.uuid1())
+        json_data = request.get_json(force=True)
+        urllib.request.urlretrieve(json_data['audio_url'], 'audio/' + request_id + '.mp3')
 
-        parser = reqparse.RequestParser()
-        parser.add_argument('audio_url')
-        parser.add_argument('dialog')
-        args = parser.parse_args()
+        try:
+            json_data['audio_url'] = 'audio/' + request_id + '.mp3'
+        except KeyError:
+            json_data['audio_url'] = 'did not receive this parameter'
 
-        urllib.request.urlretrieve(args['audio_url'], 'audio/' + request_id + '.mp3')
-
-        dialog = args['dialog']
-        write_log(request_id, request_id + ".mp3", dialog)
+        json_data['request_id'] = request_id
+        write_log(json_data)
 
         return {"response": "success"}
 
