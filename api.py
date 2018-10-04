9f3b61ed3d5dda71009bdfd3305812468a0baa50
vladimirsvsv77 <vladimirsvsv77@gmail.com>
2018-10-04 14:02:11 +0300

    added api

---
+++ api.py
@@ -0,0 +1,39 @@
+#!flask/bin/python
+import uuid
+
+import werkzeug
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+
+def write_log(request_id, file_name, log):
+    log_file = open('dialog_log.txt', 'a')
+    log_file.write('request_id: ' + request_id + ';\n')
+    log_file.write('file_name: ' + file_name + ';\n')
+    log_file.write('log: ' + log + ';\n\n')
+    log_file.close()
+
+
+class UploadAudio(Resource):
+    def post(self):
+        request_id = str(uuid.uuid1())
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
+        parser.add_argument('dialog')
+        args = parser.parse_args()
+
+        audio_file = args['file']
+        audio_file.save('audio/' + request_id + '.mp3')
+
+        dialog = args['dialog']
+        write_log(request_id, request_id + ".mp3", dialog)
+
+
+api.add_resource(UploadAudio, '/save_log')
+
+if __name__ == '__main__':
+    app.run(debug=True)
